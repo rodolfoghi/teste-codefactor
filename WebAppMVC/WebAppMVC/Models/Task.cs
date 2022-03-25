06d3d3d6f2559cc2a7a56efa53db5b7188fc54c2
Rodolfo Ghiggi <rodolfo@maxiprod.com.br>
2022-03-25 20:25:25 -0300

    Commit 1 (#1)

---
+++ WebAppMVC/WebAppMVC/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        private Dictionary<string, string> namespaceToNewPrefixMapping;
+
+        public Task(Dictionary<string, string> namespaceToNewPrefixMapping)
+        {
+            this.namespaceToNewPrefixMapping = namespaceToNewPrefixMapping;
+        }
+    }
+}
